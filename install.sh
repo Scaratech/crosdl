a6ef49bce9a466007b51685f4480e2c8b771b8b1
Scaratek <153395462+Scaratech@users.noreply.github.com>
2025-10-25 21:57:55 -0400

    oops

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e 
 
+function error() {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+    exit 1
+}
+
 if ! command -v git &> /dev/null; then
     error "git not found, please install git"
 fi
